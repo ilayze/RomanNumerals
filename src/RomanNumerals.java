7903a2218a12ceb68e80504fcb986d472bb4d042
ilayze <ilayndeganit@gmail.com>
2012-12-13 18:21:23 +0200

    Success 10 + algorithm for RomanNumerals

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,15 +1,45 @@
 
 public class RomanNumerals {
 
-	public String toRoman(int nmber) {
-		if (nmber == 1)
+	private int val[];
+	private String romanChars[];
+	public RomanNumerals()
+	{
+		val=new int[9];
+		val[0]=100;	val[1]=90;  val[2]=50;
+		val[3]=40;  val[4]=10;  val[5]=9;
+		val[6]=5;   val[7]=4;   val[8]=1;
+		
+		romanChars=new String[9];
+		romanChars[0]="C";  romanChars[1]="XC";  romanChars[2]="L";
+		romanChars[3]="XL";  romanChars[4]="X";  romanChars[5]="IX";
+		romanChars[6]="V";  romanChars[7]="IV";  romanChars[8]="I";
+		
+	}
+	public String toRoman(int number) {
+		
+		/*
+		if (number == 1)
 			return "I";
-		if(nmber==2)
+		if(number==2)
 		{
 			return "II";
 		}
 		else
 			return "III";
+			*/
+		String romanNumeral="";
+		for(int i=0;i<romanChars.length;i++)
+		{
+			while(number>=val[i])
+			{
+				number-=val[i];
+				romanNumeral+=romanChars[i];
+			}
+		}
+		return romanNumeral;
+		
 	}
-
 }
+
+
